8c460309bb78433133c4c997b592f5df91522b19
Yannick Levadoux <ylevadoux@duck.com>
2026-08-01 20:58:24 +0200

    feature/56 : add custom js script & modify mark-in-progress step

---
+++ .github/scripts/workflow-utils.js
@@ -0,0 +1,50 @@
+const summaryRows = [];
+
+/**
+ * Add a success message to the logs and the Step Summary.
+ */
+function notice(message) {
+  core.notice(message);
+  summaryRows.push(["✅", message]);
+}
+
+/**
+ * Add an informational message to the logs and the Step Summary.
+ */
+function warning(message) {
+  core.warning(message);
+  summaryRows.push(["ℹ️", message]);
+}
+
+/**
+ * Add an error message to the logs and the Step Summary.
+ */
+function failure(message) {
+  core.error(message);
+  summaryRows.push(["❌", message]);
+}
+
+/**
+ * Write the Step Summary.
+ */
+async function writeSummary(title) {
+  await core.summary
+    .addHeading(title)
+    .addTable([
+      [
+        { data: "Status", header: true },
+        { data: "Action", header: true }
+      ],
+      ...summaryRows.map(([status, action]) => [status, action])
+    ])
+    .write();
+
+    summaryRows.length = 0;
+}
+
+module.exports = {
+  notice,
+  warning,
+  failure,
+  writeSummary,
+};
